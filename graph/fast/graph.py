fdb7ec20f3dc1220f5e86d615127a704c76f2411
Seven Lju <dna2gle@gmail.com>
2015-12-07 02:31:33 +0800

    [Graph] Basic Graph Algorithm Implementation
    - DFS algorithm
    - BFS algorithm

--- graph/fast/graph.py
+++ graph/fast/graph.py
@@ -1,5 +1,3 @@
-import random
-
 class GraphNode(object):
     def __init__(self):
         self.type = 0
@@ -7,6 +5,15 @@ class GraphNode(object):
         self.index = {}
         self.hole = []
         self.nodes = []
+        self._strcache = None
+
+    def __str__(self):
+        if self._strcache is None:
+            self._strcache = str(self.data)
+        return self._strcache
+
+    def __repr__(self):
+        return self.__str__()
 
     def contains(self, node):
         return id(node) in self.index
@@ -50,7 +57,67 @@ class GraphNode(object):
         del self.index[id(node)]
         return self
 
-    def random_walk(self):
+    def _filter(self, nodes):
+        return nodes
+
+    def tranverse_bfs(self, callback=None, filter=None):
+        if filter is None:
+            filter = self._filter
+        visit = {}
+        queue = [self]
+        next_level = []
+        while len(queue) > 0:
+            for node in queue:
+                if node is None:
+                    continue
+                visit[id(node)] = -1
+                for one in filter(node.nodes):
+                    if one is None:
+                        continue
+                    if id(one) in visit:
+                        continue
+                    next_level.append(one)
+                    visit[id(one)] = -1
+                if callback is not None:
+                    callback(node)
+            queue = next_level
+            next_level = []
+
+    def tranverse_dfs(self, callback=None, filter=None):
+        if filter is None:
+            filter = self._filter
+        visit = {}
+        queue = [self]
+        next_level = []
+        visit[id(self)] = 0
+        while len(queue) > 0:
+            node = queue.pop()
+            if node is None:
+                continue
+            visit[id(node)] = -1
+            n = len(queue)
+            for one in filter(node.nodes):
+                if one is None:
+                    continue
+                if id(one) in visit:
+                    i = visit[id(one)]
+                    if i < 0 or i >= n:
+                        continue
+                    queue[i] = None
+                visit[id(one)] = n + len(next_level)
+                next_level.append(one)
+            if callback is not None:
+                callback(node)
+            next_level.reverse()
+            m = len(next_level)
+            for i, one in enumerate(next_level):
+                k = id(one)
+                visit[k] = (n + m) - (visit[k] - n) - 1
+            queue = queue + next_level
+            next_level = []
+
+    def debug_random_walk(self):
+        import random
         node = None
         self.strip()
         n = len(self.nodes) - 1
